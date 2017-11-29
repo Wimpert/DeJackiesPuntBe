6b9497af11ad0d9bf37168f0152c4d8b8ee1f7a8
Wim Holvoet <holvoetwim@hotmail.com>
2017-11-29 11:33:46 +0100

    instial project setup

---
+++ src/main/java/be/dejackies/jackiessite/GreetingController.java
@@ -0,0 +1,15 @@
+package be.dejackies.jackiessite;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+}
---
+++ src/main/java/be/dejackies/jackiessite/JackiessiteApplication.java
@@ -0,0 +1,13 @@
+package be.dejackies.jackiessite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JackiessiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JackiessiteApplication.class, args);
+
+	}
+}
