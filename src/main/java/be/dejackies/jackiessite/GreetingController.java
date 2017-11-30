77964db697fd7e536df5038639f20f01321bcf52
Wim Holvoet <holvoetwim@hotmail.com>
2017-11-30 22:13:36 +0100

    root war

--- src/main/java/be/dejackies/jackiessite/GreetingController.java
+++ src/main/java/be/dejackies/jackiessite/GreetingController.java
@@ -4,12 +4,59 @@ package be.dejackies.jackiessite;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+
+
 @RestController
 public class GreetingController {
 
     @RequestMapping("/greetings")
-    public String index() {
+    public String greet() {
         return "Greetings from Spring Boot!";
+    }
+
+    @RequestMapping("/mail")
+    public String sendMail() {
+
+        final String username = "de_jackies@hotmail.com";
+        final String password = "spelvreugde666";
+
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.live.com");
+        props.put("mail.smtp.port", "587");
+
+
+        Session session = Session.getInstance(props,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(username, password);
+                    }
+                });
+
+        try {
+
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress("de_jackies@hotmail.com"));
+            message.setRecipients(Message.RecipientType.TO,
+                    InternetAddress.parse("holvoetwim@hotmail.com"));
+            message.setSubject("Testing Subject");
+            message.setText("Dear Mail Crawler,"
+                    + "\n\n No spam to my email, please!");
+
+            Transport.send(message);
+
+            System.out.println("Done");
+
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+
+        return "Ok";
     }
 
 }
--- src/main/java/be/dejackies/jackiessite/JackiessiteApplication.java
+++ src/main/java/be/dejackies/jackiessite/JackiessiteApplication.java
@@ -1,30 +1,26 @@
 package be.dejackies.jackiessite;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
-
-@Configuration
-@ComponentScan
-@EnableAutoConfiguration
 @SpringBootApplication
 public class JackiessiteApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		System.out.println("Starting");
+		return application.sources(JackiessiteApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(JackiessiteApplication.class, args);
 
 	}
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		System.out.println("Starting");
-		return application.sources(applicationClass);
-	}
 
-	private static Class<JackiessiteApplication> applicationClass = JackiessiteApplication.class;
+
+
+
 }
