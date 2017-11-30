9956b2dd105707dcdc413c43ee8059e04428493f
Wim Holvoet <holvoetwim@hotmail.com>
2017-11-30 10:31:58 +0100

    turn into war

--- src/main/java/be/dejackies/jackiessite/JackiessiteApplication.java
+++ src/main/java/be/dejackies/jackiessite/JackiessiteApplication.java
@@ -2,12 +2,21 @@ package be.dejackies.jackiessite;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JackiessiteApplication {
+public class JackiessiteApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JackiessiteApplication.class, args);
 
 	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(applicationClass);
+	}
+
+	private static Class<JackiessiteApplication> applicationClass = JackiessiteApplication.class;
 }
