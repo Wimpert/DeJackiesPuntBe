4b9b4e32f46f3c08c401bbdf28fe49bf0ae995f9
Wim Holvoet <holvoetwim@hotmail.com>
2017-11-30 15:21:16 +0100

    static page

--- src/main/java/be/dejackies/jackiessite/GreetingController.java
+++ src/main/java/be/dejackies/jackiessite/GreetingController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 
-    @RequestMapping("/")
+    @RequestMapping("/greetings")
     public String index() {
         return "Greetings from Spring Boot!";
     }
