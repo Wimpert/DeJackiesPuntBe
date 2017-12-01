c170e8f8bb887853ba36537d8c60ffeaebbd7363
Wim Holvoet <holvoetwim@hotmail.com>
2017-12-01 21:33:17 +0100

    changed email

--- src/main/java/be/dejackies/jackiessite/GreetingController.java
+++ src/main/java/be/dejackies/jackiessite/GreetingController.java
@@ -15,14 +15,14 @@ public class GreetingController {
 
     @RequestMapping("/greetings")
     public String greet() {
-        return "Greetings from Spring Boot!";
+        return "Greetings from De Jackies!";
     }
 
     @RequestMapping("/mail")
     public String sendMail() {
 
-        final String username = "de_jackies@hotmail.com";
-        final String password = "spelvreugde666";
+        final String username = "holvoetwim@hotmail.com";
+        final String password = "rodauce2054";
 
         Properties props = new Properties();
         props.put("mail.smtp.auth", "true");
@@ -41,7 +41,7 @@ public class GreetingController {
         try {
 
             Message message = new MimeMessage(session);
-            message.setFrom(new InternetAddress("de_jackies@hotmail.com"));
+            message.setFrom(new InternetAddress("holvoetwim@hotmail.com"));
             message.setRecipients(Message.RecipientType.TO,
                     InternetAddress.parse("holvoetwim@hotmail.com"));
             message.setSubject("Testing Subject");
